5b3c252b0e5a42fa393b16a19d1dbe3d272cc4cc
Christopher Cole <christophercole1987@icloud.com>
2019-01-14 16:51:05 -0500

    Initial Commit

---
+++ bank ledger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bank ledger
+//
+//  Created by Christopher Cole on 1/14/19.
+//  Copyright © 2019 Christopher Cole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
